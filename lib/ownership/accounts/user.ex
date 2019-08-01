fcc1ca0dc178fe7612b4144f8c799cf0ead9992d
Jamal Rahman <jamalrahmanf@gmail.com>
2019-07-31 22:06:53 -0300

    set unique index for email

--- lib/ownership/accounts/user.ex
+++ lib/ownership/accounts/user.ex
@@ -2,20 +2,19 @@ defmodule Ownership.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
-    field :address, :string
-    field :cep, :string
-    field :city, :string
-    field :class, :string
-    field :cpf, :string
-    field :email, :string
-    field :name, :string
-    field :neighbourhood, :string
-    field :number, :integer
-    field :phone, :string
-    field :state, :string
-    field :type, :string
+    field(:address, :string)
+    field(:cep, :string)
+    field(:city, :string)
+    field(:class, :string)
+    field(:cpf, :string)
+    field(:email, :string, unique: true)
+    field(:name, :string)
+    field(:neighbourhood, :string)
+    field(:number, :integer)
+    field(:phone, :string)
+    field(:state, :string)
+    field(:type, :string)
 
     timestamps()
   end
@@ -23,7 +22,33 @@ defmodule Ownership.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:type, :name, :email, :cpf, :phone, :class, :cep, :address, :neighbourhood, :city, :number, :state])
-    |> validate_required([:type, :name, :email, :cpf, :phone, :class, :cep, :address, :neighbourhood, :city, :number, :state])
+    |> cast(attrs, [
+      :type,
+      :name,
+      :email,
+      :cpf,
+      :phone,
+      :class,
+      :cep,
+      :address,
+      :neighbourhood,
+      :city,
+      :number,
+      :state
+    ])
+    |> validate_required([
+      :type,
+      :name,
+      :email,
+      :cpf,
+      :phone,
+      :class,
+      :cep,
+      :address,
+      :neighbourhood,
+      :city,
+      :number,
+      :state
+    ])
   end
 end
--- priv/repo/migrations/20190731220643_create_users.exs
+++ priv/repo/migrations/20190731220643_create_users.exs
@@ -3,21 +3,22 @@ defmodule Ownership.Repo.Migrations.CreateUsers do
 
   def change do
     create table(:users) do
-      add :type, :string
-      add :name, :string
-      add :email, :string
-      add :cpf, :string
-      add :phone, :string
-      add :class, :string
-      add :cep, :string
-      add :address, :string
-      add :neighbourhood, :string
-      add :city, :string
-      add :number, :integer
-      add :state, :string
+      add(:type, :string)
+      add(:name, :string)
+      add(:email, :string)
+      add(:cpf, :string)
+      add(:phone, :string)
+      add(:class, :string)
+      add(:cep, :string)
+      add(:address, :string)
+      add(:neighbourhood, :string)
+      add(:city, :string)
+      add(:number, :integer)
+      add(:state, :string)
 
       timestamps()
     end
 
+    create(unique_index(:users, [:email]))
   end
 end
